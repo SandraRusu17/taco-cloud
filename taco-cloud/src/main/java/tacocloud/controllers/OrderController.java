4c28c1ef29b8425ea234c65faaf86c84cb31a811
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-02-09 23:20:13 +0200

    Added validation at form binding

--- taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
+++ taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
@@ -3,10 +3,13 @@ package tacocloud.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacocloud.models.data.Order;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -16,11 +19,14 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
-        return "ordersForm.html";
+        return "ordersForm";
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if(errors.hasErrors()) {
+            return "ordersForm";
+        }
         log.info("Order submitted:" + order);
         return "redirect:/";
     }
