9978a524405353146c673eae917fc212afa9b98d
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-02-12 23:32:05 +0200

    Added Spring Data JPA to the project. Annotated the domain as entities

--- taco-cloud/src/main/java/tacocloud/models/data/Taco.java
+++ taco-cloud/src/main/java/tacocloud/models/data/Taco.java
@@ -1,15 +1,21 @@
 package tacocloud.models.data;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,9 +24,12 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 
-    public Taco() {
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
     }
 }
