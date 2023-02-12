3289cafb423dd06c7bc75d9078586b97bb0ba662
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-02-12 18:31:57 +0200

    Injected OrderRepository into OrderController

--- taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
+++ taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
@@ -7,27 +7,40 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import tacocloud.models.data.Order;
+import tacocloud.models.repositories.OrderRepository;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
         return "ordersForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
-        if(errors.hasErrors()) {
+    public String processOrder(@Valid Order order,
+                               Errors errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
             return "ordersForm";
         }
-        log.info("Order submitted:" + order);
+
+        orderRepo.save(order);
+        sessionStatus.setComplete();
         return "redirect:/";
     }
 }
