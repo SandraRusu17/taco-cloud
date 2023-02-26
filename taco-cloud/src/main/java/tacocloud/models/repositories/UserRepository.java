b2b538c283b5bc96bdc63bf84b933df15871e06f
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-02-26 13:23:51 +0200

    Created User entity and repo

---
+++ taco-cloud/src/main/java/tacocloud/models/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package tacocloud.models.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import tacocloud.models.data.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
