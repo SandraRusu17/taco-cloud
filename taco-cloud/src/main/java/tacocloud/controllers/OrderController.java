56294e1b10b0e4cecd2808eb003c229d5328ed13
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-02-26 22:06:30 +0200

    Identified the authenticated user

--- taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
+++ taco-cloud/src/main/java/tacocloud/controllers/OrderController.java
@@ -1,6 +1,7 @@
 package tacocloud.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacocloud.models.data.Order;
+import tacocloud.models.data.User;
 import tacocloud.models.repositories.OrderRepository;
+import tacocloud.models.repositories.UserRepository;
 
 import javax.validation.Valid;
 
@@ -22,8 +25,11 @@ public class OrderController {
 
     private OrderRepository orderRepo;
 
-    public OrderController(OrderRepository orderRepo) {
+    private UserRepository userRepo;
+
+    public OrderController(OrderRepository orderRepo, UserRepository userRepo) {
         this.orderRepo = orderRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/current")
@@ -34,10 +40,13 @@ public class OrderController {
     @PostMapping
     public String processOrder(@Valid Order order,
                                Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "ordersForm";
         }
+
+        order.setUser(user);
 
         orderRepo.save(order);
         sessionStatus.setComplete();
--- taco-cloud/src/main/java/tacocloud/models/data/Order.java
+++ taco-cloud/src/main/java/tacocloud/models/data/Order.java
@@ -73,4 +73,7 @@ public class Order implements Serializable {
     void placedAt() {
         this.placedAt = new Date();
     }
+
+    @ManyToOne
+    private User user;
 }
